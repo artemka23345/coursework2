f129325680bd01e81dd19393ced04cb72cfa6464
Inv <artem@tarabakin.ru>
2024-01-21 23:55:05 +0300

    Start application

--- src/main/java/com/slypro/coursework2/controller/JavaController.java
+++ src/main/java/com/slypro/coursework2/controller/JavaController.java
@@ -1,4 +1,11 @@
 package com.slypro.coursework2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping
 public class JavaController {
 }
